328d5669cf5314acec2e053e0852137a7b2e02bf
Toru Hitomi <toru.hitomi@neu-brains.com>
2023-03-25 11:53:22 +0900

    updated NAMESPACE etc on 2023.03.25

--- Nobitoru_app/app.R
+++ Nobitoru_app/app.R
@@ -56,6 +56,8 @@ if (init_year == current_year) {
 # This spreadsheet has multiple sheets each of which is to be accessed
 library(googledrive)
 library(googlesheets4)
+library(future)
+
 
 drive_auth(email = "world.through.eyes@gmail.com")
 # gs4_auth(scope = "https://www.googleapis.com/auth/drive")
@@ -64,6 +66,8 @@ budget_data <- drive_get("Budget_Nobitoru_202004-")
 sheet_id <- googlesheets4::as_sheets_id(budget_data) %>% 
     sheet_properties()
 infra_data <- read_sheet(ss = budget_data, sheet = "Infra")
+
+future::plan(multisession, workers = availableCores() - 1)
 monthly <- sheetids %>% 
     furrr::future_map_dfr(.f = read_sheet, .id = "sheetid",
     # purrr::map_dfr(.f = read_sheet, 
@@ -142,13 +146,13 @@ etc.files <- fs::dir_ls(path = etc.dir, type = "file", regexp = ".csv$$")
 if (length(etc.files) == 0) {
     # do nothing
 } else if (length(etc.files) > 0) {
-    etc.files <- sort(etc.files)[1]
+    etc.files <- sort(etc.files)[length(etc.files)]
     etc.df <- read.csv(etc.files, header = F, skip = 1, fileEncoding = "cp932") %>% 
         tibble::as_tibble() %>% 
         dplyr::select(V3, V4, V5, V6, V11) %>% 
         magrittr::set_colnames(c("date", "time", "in_name" , "out_name", "price"))
 }
-
+# etc.df
     
 
 
@@ -189,7 +193,8 @@ ui <- navbarPage(
              fluidRow(
                  column(12, p(
                      "History:
-                              2021.10 - 2022.02 Living in Fujimi Town, Nagano, supported by Nagano Pref. (Otameshi Nagano Program)"
+                              2021.10 - 2022.02 Living in Fujimi Town, Nagano, supported by Nagano Pref. (Otameshi Nagano Program)
+                              2023.05 -         Purchased a house (¥8M) and started living in Fujimi Town, Nagano"
                  ))
              ))
 )
@@ -222,7 +227,7 @@ server <- function(input, output) {
             ggplot(aes(x = Date, y = Data, color = Nobitoru, group = Nobitoru)) +
             geom_point() +
             geom_line() +
-            geom_hline(data = avg, aes(yintercept = meanData, color = Nobitoru, group = Nobitoru), lty = "dotted", size = 1) +
+            geom_hline(data = avg, aes(yintercept = meanData, color = Nobitoru, group = Nobitoru), lty = "dotted", linewidth = 1) +
             facet_grid(rows = vars(DataType), scales = "free")
         
         if (input$who == "Both") {
@@ -244,13 +249,15 @@ server <- function(input, output) {
             ungroup() %>% 
             pivot_longer(cols = Bank_Mizuho:Total, names_to = "Bank", values_to = "Balance") %>% 
             ggplot(aes(x = Date, y = Balance/1000000, color = Bank, group = Bank)) +
-            geom_point() +
-            geom_line() + 
-            geom_smooth(method = "lm", formula = y ~ x) +
+            geom_point(na.rm = T) +
+            geom_line(na.rm = T) + 
+            geom_smooth(method = "lm", formula = y ~ x, na.rm = T) +
             theme_bw() +
-            theme(axis.text.x = element_text(angle = 90)) +
-            scale_color_manual(values = c("#1BD158", "#417CFC", "red")) +
-            labs(y = "Balance (M JPY)", title = "Toru's Bank Balance")
+            theme(axis.text.x = element_text(angle = 90),
+                  legend.position = "top") +
+            scale_color_manual(values = c("#1BD158", "#417CFC", "#00CDCD", "red")) +
+            labs(y = "Balance (M JPY)", title = "Nobitoru's Bank Balance in Japan",
+                 caption = "Wealths outside Japan are not considered.")
         ggplotly(budget_track)
     })
     
@@ -260,7 +267,7 @@ server <- function(input, output) {
             mutate(meanCredit = -1 * mean(Bank_Mizuho)) %>% 
             ggplot(aes(x = Month, y = -1 * Bank_Mizuho)) +
             geom_bar(stat = "identity", fill = c("#00B2EE"), alpha = .6) +
-            geom_hline(aes(yintercept = meanCredit), color = "blue", lty = "dashed", size = 1.5) +
+            geom_hline(aes(yintercept = meanCredit), color = "blue", lty = "dashed", linewidth = 1.5) +
             geom_line(aes(group = 1)) +
             geom_point(size = 1.2) +
             labs(x = NULL, y = NULL, title = "Credit payment") +
